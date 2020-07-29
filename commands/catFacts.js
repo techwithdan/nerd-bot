958f2b8614747b503e031df7a289c19bf496f295
Tonegawa94 <65327047+Tonegawa94@users.noreply.github.com>
2020-07-29 00:00:55 -0700

    Update catFacts.js

--- commands/catFacts.js
+++ commands/catFacts.js
@@ -3,8 +3,8 @@ const { randomGenerator } = require("../helper_func/randomGenerator");
 
 async function catFacts(msg){
     const response = await Axios.get("https://cat-fact.herokuapp.com/facts")
-    let num = response.data.all.length
-    let index = randomGenerator(num)
+    const num = response.data.all.length
+    const index = randomGenerator(num)
     msg.channel.send(response.data.all[index].text)
 }
 
